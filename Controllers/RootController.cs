7b45e2fc6c89c5eccd3a7f8f53a833a10b891533
huxaiphaer <l654321123>
2019-01-31 15:08:25 +0300

    create root controller

---
+++ Controllers/RootController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+       [HttpGet(Name = nameof(GetRoot)]
+       public IActionResult GetRoot()
+        {
+            var response = new { href = Url.Link(nameof(GetRoot), null};
+            return Ok();
+        }
+    }
+}
